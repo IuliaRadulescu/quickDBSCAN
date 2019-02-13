5508602842b9313e7987ebafc243473cbcd5662f
LillyBlackWhite <iulia@bd-server2.pub.ro>
2019-02-13 15:45:37 +0200

    Added DBSCAN part

--- src/quickDBSCAN.py
+++ src/quickDBSCAN.py
@@ -17,7 +17,7 @@ import pymongo
 
 import mongoConnect
 
-class quickDBSCAN:
+class QuickJOIN:
 	
 	def __init__(self, filePath):
 		img = cv2.imread(filePath, cv2.IMREAD_COLOR)
@@ -40,14 +40,18 @@ class quickDBSCAN:
 					print(pixel1, pixel2)
 					#insert into Mongo
 					self.upsertPixelValue("quickDBSCAN",{"object":pixel1.tolist()}, pixel2.tolist())
+					self.upsertPixelValue("quickDBSCAN",{"object":pixel2.tolist()}, pixel1.tolist())
 
 	def nestedLoop2(self, eps, objs1, objs2):
+		print("Nested loop 2")
 		for pixel1 in objs1:
 			for pixel2 in objs2:
 				if( (pixel1 == pixel2).all() == False and self.euclideanDist(pixel1, pixel2) <= eps):
+					print("Adauga")
 					print(pixel1, pixel2)
 					#insert into Mongo
-					self.mongoConnectInstance.upsertPixelValue("quickDBSCAN",{"object":pixel1.tolist()}, pixel2.tolist())
+					self.upsertPixelValue("quickDBSCAN",{"object":pixel1.tolist()}, pixel2.tolist())
+					self.upsertPixelValue("quickDBSCAN",{"object":pixel2.tolist()}, pixel1.tolist())
 
 
 	def randomObject(self, objs, pixel):
@@ -59,13 +63,21 @@ class quickDBSCAN:
 
 		return objs[randomIndex]
 
+	def ball_average(self, objs, p1):
+		avgDistHelper = []
+		for pixel in objs:
+			if( (pixel==p1).all() == False ):
+				avgDistHelper.append(self.euclideanDist(pixel, p1))
+		avgDistHelper = np.array(avgDistHelper)
+		return sum(avgDistHelper)/len(avgDistHelper)
+
 					
 	def partition(self, eps, objs, p1, p2):
 		partL = [] 
 		partG = []
 		winL = []
 		winG = []
-		r = self.euclideanDist(p1, p2)
+		r = self.ball_average(objs, p1)
 		startIdx = 0
 		endIdx = len(objs)-1
 		startDist = self.euclideanDist(objs[startIdx], p1)
@@ -122,6 +134,7 @@ class quickDBSCAN:
 		self.quickJoin(partG, eps, constSmallNumber)
 
 	def quickJoinWin(self, objs1, objs2, eps, constSmallNumber):
+		print("Intra in win")
 		totalLen = len(objs1) + len(objs2)
 		if(totalLen < constSmallNumber):
 			self.nestedLoop2(eps, objs1, objs2)
@@ -139,7 +152,7 @@ class quickDBSCAN:
 		self.quickJoinWin(partG1, partG2, eps, constSmallNumber)
 
 	def upsertPixelValue(self, collection, filter, epsNeigh):
-		print(filter)
+		#print(filter)
 		pixelRecord = self.mongoConnectInstance.getRecord("quickDBSCAN", filter, ["_id", "object", "epsNeighs"])
 		
 		newNeighs = {'epsNeighs':list()}
@@ -157,13 +170,70 @@ class quickDBSCAN:
 			newNeighs['epsNeighs'].append(epsNeigh)
 
 		self.mongoConnectInstance.update("quickDBSCAN", filter, {"$set":newNeighs})
+			
+
+class DBSCAN:
+	def __init__(self, eps, minPts):
+		self.eps = eps
+		self.minPts = minPts
+		self.label = 0
+		self.pixelLabels = collections.defaultdict(list);
+		self.visitedPixels = []
+		self.mongoConnectInstance = mongoConnect.MongoDBConnector("QuickDBScanDB");
+
+	def checkIfPixelInList(self, pixel, objList):
+		for pixelList in objList:
+			if( (pixel==pixelList).all() == True ):
+				return True
+		return False
+
+
+	def dbscan(self, objs):
+		for pixel in objs:
+			if(self.checkIfPixelInList(pixel, self.visitedPixels) == False):
+				pixelRecord = self.mongoConnectInstance.getRecord("quickDBSCAN", {'object':pixel.tolist()}, ["_id", "object", "epsNeighs"])
+				if(pixelRecord is not None):
+					self.visitedPixels.append(pixel);
+					spherePoints = pixelRecord["epsNeighs"]
+					spherePoints = [np.asarray(tuple(p)) for p in spherePoints]
+					#print(spherePoints)
+					#print("len Sphere "+str(len(spherePoints)))
+					if(len(spherePoints) >= self.minPts):
+						self.label = self.label + 1
+						print("Cluster "+str(self.label))
+						self.pixelLabels[tuple(pixel)] = self.label
+						self.expandCluster(spherePoints)
+		return self.pixelLabels
+
+	def expandCluster(self, spherePoints):
+		print("Intra in expand")
+		for pixel in spherePoints:
+			#print(self.visitedPixels)
+			#print("pixelul "+str(pixel)+" continut in "+str(self.checkIfPixelInList(pixel, self.visitedPixels)))
+			if(self.checkIfPixelInList(pixel, self.visitedPixels) == False):
+				print("Trece de check")
+				self.visitedPixels.append(pixel)
+				self.pixelLabels[tuple(pixel)] = self.label
+				pixelRecord = self.mongoConnectInstance.getRecord("quickDBSCAN", {'object':pixel.tolist()}, ["_id", "object", "epsNeighs"])
+				if(pixelRecord is not None):
+					print("Intra")
+					spherePointsNeighs = pixelRecord["epsNeighs"]
+					spherePointsNeighs = [np.asarray(tuple(p)) for p in spherePointsNeighs]
+					print("len Sphere vecini "+str(len(spherePointsNeighs)))
+					if(len(spherePointsNeighs) >= self.minPts):
+						self.expandCluster(spherePointsNeighs)
+		return
 
 
 if __name__ == '__main__':
 
 	filePath = sys.argv[1]
 		
-	quickDBSCANInstance = quickDBSCAN(filePath)
-	quickDBSCANInstance.quickJoin(quickDBSCANInstance.pixelList, 10, 100)
-	quickDBSCANInstance.mongoConnectInstance.closeConection()
+	quickJoinInstance = QuickJOIN(filePath)
+	'''quickJoinInstance.quickJoin(quickJoinInstance.pixelList, 10, 100)
+	quickJoinInstance.mongoConnectInstance.closeConection()'''
+	dbscanInstance = DBSCAN(10, 7)
+	labelDict = dbscanInstance.dbscan(quickJoinInstance.pixelList)
+	print(len(labelDict))
+	print(set(labelDict.values()))
 
